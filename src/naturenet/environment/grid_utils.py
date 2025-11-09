6e3a895f2474539fa66476f387b3adaedae12679
nlahaye <nlahaye@jpl.nasa.gov>
2025-11-09 20:00:41 +0000

    Adding utils to split time series up, given large gaps

--- src/naturenet/environment/grid_utils.py
+++ src/naturenet/environment/grid_utils.py
@@ -1,15 +1,30 @@
 
-
-from astropy_healpix import HEALPix
-from astropy import units as u
-
+import numpy as np
+import pandas
+import os
+import pickle
 
 #TODO Grid object
+class Grid(Object):
+    def __init__(self, min_lon, min_lat, lon_size, lat_size, grid_res_lat_deg, grid_res_lon_deg):
+        self.min_lon = min_lon
+        self.min_lat = min_lat
+        self.lat_size = lat_size
+        self.lon_size = lon_size
+ 
+        self.grid_res_lat_deg = grid_res_lat_deg
+        self.grid_res_lon_deg = grid_res_lon_deg
+        
+        self.lat_tiles = lat_size / grid_res_lat_deg
+        self.lon_tiles = lon_size / grid_res_lon_deg
+
+        self.trans_prob = None
+
 
 def lat_lon_to_grid(grid, lat, lon):
 
-    x = int((lon - grid.min_lon)/ grid.lon_size)
-    y = int((lat - grid.min_lat) / grid.lat_size)
+    x = int((lon - grid.min_lon)/ grid.grid_res_lon_deg)
+    y = int((lat - grid.min_lat) / grid.grid_res_lat_deg)
     return (x, y)
 
 
@@ -42,6 +57,38 @@ def compute_transition_probabilities(movement_df, grid):
 
     return grid
 
+
+def split_movement_streams(movement_df):
+
+     movement_df = movement_df.sort_values(by=['id', 'date'])
+     dfs_by_id = [x for _, x in movement_df.groupby(movement_df["id"])]
+     first_ind = 0
+     final_dfs = {}
+     for i in range(len(dfs_by_id)):
+         for index, row in dfs_by_id[i].iterrows():
+             if len(row["date"]) == 10:
+                 row["date"] = row["date"] + " 00:00:00"
+             if index < 1:
+                 last_date = datetime.strptime(row["date"], "%Y-%m-%d %H:%M:%S")
+                 final_dfs[row["id"]] = []
+                 continue
+             current_date = datetime.strptime(row["date"], "%Y-%m-%d %H:%M:%S")
+             date_diff = current_date - last_date
+             if int(round((date_diff / 24*60*60))) > 2:
+                 final_dfs[row["id"]].append(dfs_by_id[i].iloc[first_ind:i]
+                 first_ind = i
+             last_date = current_date
+         if first_ind < len(dfs_by_id[i]):
+                 final_dfs[row["id"]].append(dfs_by_id[i].iloc[first_ind:])
+         first_ind = 0
+         
+     for key in final_dfs:
+         for i in range(len(final_dfs[key]):
+             print(key, len(final_dfs[key][i]))
+
+     with open('hammerhead_dfs.pkl') as f:
+         pickle.dump(final_dfs, f)
+     
 
 
 
