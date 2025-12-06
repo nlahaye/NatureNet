4f931e4d1897adf30f18736f2ec99e0a4fc1b5b9
nlahaye <nlahaye@jpl.nasa.gov>
2025-12-06 18:46:38 +0000

    Adding SWIRL functionality

---
+++ src/naturenet/models/swirl/run_swirl.py
@@ -0,0 +1,225 @@
+
+#Code adapted from https://github.com/BRAINML-GT/SWIRL
+from naturenet.models.swirl.run_arhmm import run_arhmm
+from naturenet.models.swirl.swirl_training import *
+from naturenet.models.swirl.swirl_training_top_level import *
+from naturenet.models.swirl.swirl_utils import *
+
+import numpy as np
+import numpy.random as npr
+from scipy.special import logsumexp
+
+import sparse
+
+import jax
+import jax.numpy as jnp
+from jax import lax, vmap, jit
+from jax.lib import xla_bridge
+from functools import partial
+from jax.scipy.special import logsumexp as jax_logsumexp
+import optax
+jax.config.update("jax_enable_x64", True)
+jax.config.update("jax_platform_name", "gpu")
+
+from sit_fuse.utils import read_yaml
+
+import argparse
+
+ 
+#K = 2 number of hidden states
+#D_obs = 1 number of observed dimensions
+#D = number of latent dimensions
+#C = 25 number of states
+
+def run_swirl_init(yml_conf):
+
+    seed = yml_conf["seed"]
+    
+    n_hidden_init = yml_conf["n_hidden_init"]
+
+    trans_prob_fpath = yml_conf["trans_probs"]
+    actions_fpath = yml_conf["actions"]
+    positions_fpath = yml_conf["positions"]
+    out_dir = yml_conf["out_dir"]
+    run_uid = yml_conf["run_uid"]
+
+    trans_prob = sparse.load_npz(trans_prob_fpath)
+    actions = np.load(actions_fpath)
+    positions = np.load(positions_fpath)
+
+    n_states, n_actions, _ = trans_probs.shape
+
+    #TODO train test split
+
+    r1 = np.zeros((n_states, n_actions))
+    r2 = np.zeros((n_states, n_actions))
+
+    np.random.seed(seed)
+    if n_hidden_init == 3:
+        r3 = npr.rand(n_states)[:, None]
+        r3 = jnp.tile(r3, (1, n_actions))
+        R_start = np.array([r1, r2, r3])
+        R_start2 = R_start.mean(axis=-1)
+    elif n_hidden_init == 2:
+        r3 = npr.rand(n_states)[:, None]
+        r3 = jnp.tile(r3, (1, n_actions))
+        R_start = np.array([r1, r3])
+        R_start2 = R_start.mean(axis=-1)
+
+  
+    arhmm_s_fname = os.path.join(out_dir, run_uid + "_" + str(n_hidden_init) + "_hidden_" + + str(seed) + '_seed_arhmm_s.npz')
+    arhmm_s_params = np.load(arhmm_s_fname, allow_pickle=True)
+    logpi0_start = arhmm_s_params['logpi0_start']
+    log_Ps_start = arhmm_s_params['log_Ps_start']
+    Rs_start = arhmm_s_params['W1_start'], arhmm_s_params['b1_start'], arhmm_s_params['W2_start'], arhmm_s_params['b2_start']
+ 
+    all_xs_prev = preprocess_xs_prev_np(positions[:, 1:], positions[:, :-1], prev_state_map)
+    all_xohs = vmap(one_hotx_partial)(positions[:, 1:])
+    all_xohs_prev = vmap(one_hotx_partial)(positions[:, :-1])
+    all_xohs2 = vmap(one_hotx2_partial)(positions[:, 1:], all_xs_prev)
+    all_aohs = vmap(one_hota_partial)(actions[:, 1:])
+  
+    print(xla_bridge.get_backend().platform)
+    temps = jnp.array([1] + [1] * (n_hidden - 1))
+
+    # S-1
+    new_logpi0, new_log_Ps, new_Rs, new_reward, LL_list = em_train_temp(jnp.array(logpi0_start), jnp.array(log_Ps_start), Rs_start, jnp.array(R_start2)[:, None], temps, 50, init=False, trans=False)
+    fname = run_uid + "_time_interval_run_" + str(n_hidden_init) + '_' + str(seed) + "_naturenet_init0.npz"
+    fname = os.path.join(out_dir, fname)
+    jnp.savez(fname, new_logpi0=new_logpi0, new_log_Ps=new_log_Ps, new_Rs=np.array(new_Rs, dtype=object), new_reward=new_reward, LL_list=LL_list, temps=temps) 
+
+    new_logpi0, new_log_Ps, new_Rs, new_reward, LL_list = em_train_temp(jnp.array(new_logpi0), jnp.array(new_log_Ps), new_Rs, jnp.array(new_reward), temps, 30)
+    new_reward = normalize_reward(new_reward)
+    new_reward = new_reward[[1, 0, 2], ...] #TODO - viz this
+    fname = run_uid + "_time_interval_run_" + str(n_hidden_init) + '_' + str(seed) + "_naturenet_init1.npz"
+    fname = os.path.join(out_dir, fname)
+    jnp.savez(fname, new_logpi0=new_logpi0, new_log_Ps=new_log_Ps, new_Rs=np.array(new_Rs, dtype=object), new_reward=new_reward, LL_list=LL_list, temps=temps)
+
+    #TODO connect observed environmental conditions to emissions / emissions dimensionality
+
+
+def run_swirl_final(yml_conf):
+
+    seed = yml_conf["seed"]
+
+    n_hidden = yml_conf["n_hidden"]
+    emission_dim = yml_conf["emission_dim"]
+
+    trans_prob_fpath = yml_conf["trans_probs"]
+    actions_fpath = yml_conf["actions"]
+    positions_fpath = yml_conf["positions"]
+    out_dir = yml_conf["out_dir"]
+ 
+    run_uid = yml_conf["run_uid"]
+    arhmm_s_fname = os.path.join(out_dir, run_uid + "_" + str(n_hidden_init) + "_hidden_" + + str(seed) + '_seed_arhmm_s.npz')
+    time_interval_params = run_uid + "_time_interval_run_" + str(n_hidden) + '_' + str(seed) + "_naturenet_init1.npz"
+
+    trans_prob = sparse.load_npz(trans_prob_fpath)
+    actions = np.load(actions_fpath)
+    positions = np.load(positions_fpath)
+
+    n_states, n_actions, _ = trans_probs.shape
+
+    time_interval_learnt_params = jnp.load(time_interval_params, allow_pickle=True)
+    init_reward = time_interval_learnt_params['new_reward']
+
+    arhmm_s_params = np.load(arhmm_s_fname)
+    logpi0_start = arhmm_s_params['arr_0']
+    log_Ps_start = arhmm_s_params['arr_1']
+    Rs_start = arhmm_s_params['arr_2'], arhmm_s_params['arr_3'], arhmm_s_params['arr_4'], arhmm_s_params['arr_5']
+  
+    r1 = init_reward[0].T
+    r1 = normalize(r1)
+    r1 = np.tile(r1, (1, n_actions))
+    r2 = init_reward[1].T
+    r2 = normalize(r2)
+    r2 = np.tile(r2, (1, n_actions))
+    np.random.seed(seed)
+    #TODO - generalize this functionality for n_hidden = some K
+    if n_hidden == 3:
+        r3 = npr.rand(n_states)[:, None]
+        r3 = jnp.tile(r3, (1, n_actions))
+        R_start = np.array([r1, r2, r3])
+        R_start2 = R_start.mean(axis=-1)
+    elif n_hidden == 2:
+        R_start = np.array([r1, r2])
+        R_start2 = R_start.mean(axis=-1)
+    elif n_hidden == 4:
+        r3 = npr.rand(n_states)[:, None]
+        r3 = jnp.tile(r3, (1, n_actions))
+        r4 = npr.rand(n_states)[:, None]
+        r4 = jnp.tile(r4, (1, n_actions))
+        R_start = np.array([r1, r2, r3, r4])
+        R_start2 = R_start.mean(axis=-1)
+    elif n_hidden == 5:
+        r3 = npr.rand(n_states)[:, None]
+        r3 = jnp.tile(r3, (1, n_actions))
+        r4 = npr.rand(n_states)[:, None]
+        r4 = jnp.tile(r4, (1, n_actions))
+        r5 = npr.rand(n_states)[:, None]
+        r5 = jnp.tile(r5, (1, n_actions))
+        R_start = np.array([r1, r2, r3, r4, r5])
+        R_start2 = R_start.mean(axis=-1)
+
+    
+    all_xs_prev = preprocess_xs_prev_np(positions[:, 1:], positions[:, :-1], prev_state_map)
+    all_xohs = vmap(one_hotx_partial)(positions[:, 1:])
+    all_xohs_prev = vmap(one_hotx_partial)(positions[:, :-1])
+    all_xohs2 = vmap(one_hotx2_partial)(positions[:, 1:], all_xs_prev)
+    all_aohs = vmap(one_hota_partial)(actions[:, 1:])
+
+    print(xla_bridge.get_backend().platform)
+    temps = jnp.array([0.01] + [1] * (n_hidden - 1))
+
+    # S-2
+    new_logpi02, new_log_Ps2, new_Rs2, new_reward2, LL_list2 = em_train2_labyrinth(jnp.array(logpi0_start), jnp.array(log_Ps_start), Rs_start, jnp.array(R_start)[:, None].reshape(n_hidden, emission_dim, n_states*n_actions), 50)
+    fname = run_uid + "_" + str(n_hidden_init) + '_' + str(seed) + "_naturenet_iter2_init.npz" 
+    fname = os.path.join(out_dir, fname) 
+    jnp.savez(fname, new_logpi0=new_logpi02, new_log_Ps=new_log_Ps2, new_Rs=np.array(new_Rs2, dtype=object), new_reward=new_reward2, LL_list=LL_list2, temps=temps)
+    new_logpi02, new_log_Ps2, new_Rs2, new_reward2, LL_list2 = em_train2_temp(jnp.array(new_logpi02), jnp.array(new_log_Ps2), new_Rs2, jnp.array(new_reward2), temps, 30)
+    fname = run_uid + "_" + str(n_hidden_init) + '_' + str(seed) + "_naturenet_iter2.npz"
+    fname = os.path.join(out_dir, fname)
+    jnp.savez(fname, new_logpi0=new_logpi02, new_log_Ps=new_log_Ps2, new_Rs=np.array(new_Rs2, dtype=object), new_reward=new_reward2, LL_list=LL_list2, temps=temps)
+ 
+    # # S-1
+    new_logpi0, new_log_Ps, new_Rs, new_reward, LL_list = em_train_labyrinth(jnp.array(logpi0_start), jnp.array(log_Ps_start), Rs_start, jnp.array(R_start2)[:, None], 50)
+    fname = run_uid + "_" + str(n_hidden_init) + '_' + str(seed) + "_naturenet_iter2_temp_init.npz"
+    fname = os.path.join(out_dir, fname)
+    jnp.savez(fname, new_logpi0=new_logpi0, new_log_Ps=new_log_Ps, new_Rs=np.array(new_Rs, dtype=object), new_reward=new_reward, LL_list=LL_list, temps=temps)
+
+    new_logpi0, new_log_Ps, new_Rs, new_reward, LL_list = em_train_temp(jnp.array(new_logpi0), jnp.array(new_log_Ps), new_Rs, jnp.array(new_reward), temps, 30)
+    fname = run_uid + "_" + str(n_hidden_init) + '_' + str(seed) + "_naturenet_iter2_temp_2.npz"
+    fname = os.path.join(out_dir, fname)
+    jnp.savez(fname, new_logpi0=new_logpi0, new_log_Ps=new_log_Ps, new_Rs=np.array(new_Rs, dtype=object), new_reward=new_reward, LL_list=LL_list, temps=temps)
+ 
+    # Load S-2 params
+    fname = run_uid + "_" + str(n_hidden_init) + '_' + str(seed) + "_naturenet_iter2.npz"
+    fname = os.path.join(out_dir, fname)
+    params2 = jnp.load(fname, allow_pickle=True)
+    new_logpi02, new_log_Ps2, new_Rs2, new_reward2, LL_list2 = params2['new_logpi0'], params2['new_log_Ps'], params2['new_Rs'], params2['new_reward'], params2['LL_list']
+
+  
+    #TODO - change to all, train, test
+    LL, train_LL, test_LL, jax_path_vmap = learnt_LL2(new_logpi02, new_log_Ps2, new_Rs2, new_reward2, temps, n_states, n_actions, trans_probs, all_xohs, all_aohs, all_xohs, all_aohs, all_aohs, all_xohs)
+    print(LL, train_LL, test_LL)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-y", "--yaml", help="YAML file for fusion info.")
+    args = parser.parse_args()
+
+    #Translate config to dictionary 
+    yml_conf = read_yaml(args.yaml)
+ 
+    if yml_conf["run_arhmm"]:
+        run_arhmm(yml_conf)    
+
+    if yml_conf["run_swirl_init"]:
+        run_swirl_init(yml_conf)
+ 
+    if yml_conf["run_swirl_final"]:
+        run_swirl_final(yml_conf)
+
+
+
---
+++ src/naturenet/models/swirl/swirl_training_top_level.py
@@ -0,0 +1,174 @@
+
+#Code adapted from https://github.com/BRAINML-GT/SWIRL
+
+import numpy as np
+import numpy.random as npr
+from scipy.special import logsumexp
+
+import sparse
+
+import jax
+import jax.numpy as jnp
+from jax import lax, vmap, jit
+from functools import partial
+from jax.scipy.special import logsumexp as jax_logsumexp
+import optax
+ 
+from naturenet.models.swirl.swirl_training import *
+from naturenet.models.swirl.swirl_utils import *
+
+jax.config.update("jax_enable_x64", True)
+jax.config.update("jax_platform_name", "gpu")
+
+def em_train_temp(logpi0, log_Ps, Rs, rewards, temps, iter=100, init=True, trans=True, emit=True):
+    LL_list = []
+    for i in range(iter):
+        print(i)
+        pi0 = jnp.exp(logpi0 - jax_logsumexp(logpi0))
+        all_gamma_jax, all_xi_jax, all_jax_alphas = jax_e_step_batch_temp(pi0, log_Ps, Rs, rewards, trans_probs, temps, train_xohs, train_aohs)
+        print(jnp.sum(jax_logsumexp(all_jax_alphas[:, -1], axis=-1)))
+        LL_list.append(jnp.sum(jax_logsumexp(all_jax_alphas[:, -1], axis=-1)))
+
+        if init == True:
+            new_logpi0 = pi0_m_step(all_gamma_jax)
+        else:
+            new_logpi0 = logpi0
+        print(new_logpi0)
+
+        if trans == True:
+            new_log_Ps, new_Rs = trans_m_step_jax_scipy2(log_Ps, Rs, (all_gamma_jax, all_xi_jax), jnp.array(train_xohs))
+        else:
+            new_log_Ps, new_Rs = log_Ps, Rs
+
+        if emit == True:
+            new_rewards = emit_m_step_jax_scipy_temp_reg(rewards, trans_probs, temps,(all_gamma_jax, all_xi_jax), jnp.array(train_xohs), jnp.array(train_aohs))
+        else:
+            new_rewards = rewards
+
+        logpi0, log_Ps, Rs, rewards = new_logpi0, new_log_Ps, new_Rs, new_rewards
+    return logpi0, log_Ps, Rs, rewards, LL_list
+
+def em_train2_temp(logpi0, log_Ps, Rs, rewards, temps, iter=100, init=True, trans=True, emit=True):
+    LL_list = []
+    for i in range(iter):
+        print(i)
+        pi0 = jnp.exp(logpi0 - jax_logsumexp(logpi0))
+        all_gamma_jax, all_xi_jax, all_jax_alphas = jax_e_step_batch2_temp(pi0, log_Ps, Rs, rewards, new_trans_probs, temps, train_xohs, train_xohs2, train_aohs)
+        print(jnp.sum(jax_logsumexp(all_jax_alphas[:, -1], axis=-1)))
+        LL_list.append(jnp.sum(jax_logsumexp(all_jax_alphas[:, -1], axis=-1)))
+
+        if init == True:
+            new_logpi0 = pi0_m_step(all_gamma_jax)
+        else:
+            new_logpi0 = logpi0
+        print(new_logpi0)
+
+        if trans == True:
+            new_log_Ps, new_Rs = trans_m_step_jax_scipy2(log_Ps, Rs, (all_gamma_jax, all_xi_jax), jnp.array(train_xohs))
+        else:
+            new_log_Ps, new_Rs = log_Ps, Rs
+
+        if emit == True:
+            new_rewards = emit_m_step_jax_scipy2_temp(rewards, new_trans_probs, temps, (all_gamma_jax, all_xi_jax), jnp.array(train_xohs2), jnp.array(train_aohs))
+        else:
+            new_rewards = rewards
+
+        logpi0, log_Ps, Rs, rewards = new_logpi0, new_log_Ps, new_Rs, new_rewards
+    return logpi0, log_Ps, Rs, rewards, LL_list
+
+
+def em_train_naturenet(logpi0, log_Ps, Rs, rewards, iter=100, init=True, trans=True, emit=True):
+    LL_list = []
+    for i in range(iter):
+        print(i)
+        pi0 = jnp.exp(logpi0 - jax_logsumexp(logpi0))
+        all_gamma_jax, all_xi_jax, all_jax_alphas = jax_e_step_batch_labyrinth(pi0, log_Ps, Rs, rewards, trans_probs, train_xohs, train_aohs)
+        print(jnp.sum(jax_logsumexp(all_jax_alphas[:, -1], axis=-1)))
+        LL_list.append(jnp.sum(jax_logsumexp(all_jax_alphas[:, -1], axis=-1)))
+
+        if init == True:
+            new_logpi0 = pi0_m_step(all_gamma_jax)
+        else:
+            new_logpi0 = logpi0
+        print(new_logpi0)
+
+        if trans == True:
+            new_log_Ps, new_Rs = trans_m_step_jax_scipy2(log_Ps, Rs, (all_gamma_jax, all_xi_jax), jnp.array(train_xohs))
+        else:
+            new_log_Ps, new_Rs = log_Ps, Rs
+
+        if emit == True:
+            new_rewards = emit_m_step_jax_scipy2_labyrinth(rewards, trans_probs, (all_gamma_jax, all_xi_jax), jnp.array(train_xohs), jnp.array(train_aohs))
+        else:
+            new_rewards = rewards
+
+        logpi0, log_Ps, Rs, rewards = new_logpi0, new_log_Ps, new_Rs, new_rewards
+    return logpi0, log_Ps, Rs, rewards, LL_list
+
+def em_train2_naturenet(logpi0, log_Ps, Rs, rewards, iter=100, init=True, trans=True, emit=True):
+    LL_list = []
+    for i in range(iter):
+        print(i)
+        pi0 = jnp.exp(logpi0 - jax_logsumexp(logpi0))
+        all_gamma_jax, all_xi_jax, all_jax_alphas = jax_e_step_batch2_labyrinth(pi0, log_Ps, Rs, rewards, new_trans_probs, train_xohs, train_xohs2, train_aohs)
+        print(jnp.sum(jax_logsumexp(all_jax_alphas[:, -1], axis=-1)))
+        LL_list.append(jnp.sum(jax_logsumexp(all_jax_alphas[:, -1], axis=-1)))
+
+        if init == True:
+            new_logpi0 = pi0_m_step(all_gamma_jax)
+        else:
+            new_logpi0 = logpi0
+        print(new_logpi0)
+
+        if trans == True:
+            new_log_Ps, new_Rs = trans_m_step_jax_scipy2(log_Ps, Rs, (all_gamma_jax, all_xi_jax), jnp.array(train_xohs))
+        else:
+            new_log_Ps, new_Rs = log_Ps, Rs
+
+        if emit == True:
+            new_rewards = emit_m_step_jax_scipy2_labyrinth(rewards, new_trans_probs, (all_gamma_jax, all_xi_jax), jnp.array(train_xohs2), jnp.array(train_aohs))
+           
+        else:
+            new_rewards = rewards
+
+        logpi0, log_Ps, Rs, rewards = new_logpi0, new_log_Ps, new_Rs, new_rewards
+    return logpi0, log_Ps, Rs, rewards, LL_list
+
+
+
+def comp_LLloss(pi0, trans_Ps, lls):
+    alphas_list = vmap(partial(forward, jnp.array(pi0)))(trans_Ps, lls)
+    return jnp.sum(jax_logsumexp(alphas_list[:, -1], axis=-1))
+
+def learnt_LL1(logpi0, log_Ps, Rs, rewards, temps, n_states, n_actions, trans_probs, all_xohs, all_aohs, train_xohs, train_aohs, test_aohs, test_xohs):
+    pi0 = jnp.exp(logpi0 - jax_logsumexp(logpi0))
+    rewards_sa = jnp.expand_dims(rewards[:, 0, :], axis=2) * np.ones((1, n_states, n_actions))
+    pi, _, _ = vmap(partial(vi_temp, trans_probs))(rewards_sa, temps)
+    logemit = jnp.log(pi)
+    new_lls_jax_vmap = vmap(partial(comp_ll_jax, logemit))(jnp.array(all_xohs), jnp.array(all_aohs))
+    new_trans_Ps_vmap = vmap(partial(comp_transP, jnp.array(log_Ps), Rs))(jnp.array(all_xohs))
+    new_lls_jax_vmap_train = vmap(partial(comp_ll_jax, logemit))(jnp.array(train_xohs), jnp.array(train_aohs))
+    new_trans_Ps_vmap_train = vmap(partial(comp_transP, jnp.array(log_Ps), Rs))(jnp.array(train_xohs))
+    new_lls_jax_vmap_test = vmap(partial(comp_ll_jax, logemit))(jnp.array(test_xohs), jnp.array(test_aohs))
+    new_trans_Ps_vmap_test = vmap(partial(comp_transP, jnp.array(log_Ps), Rs))(jnp.array(test_xohs))
+    jax_path_vmap = vmap(partial(_viterbi_JAX, jnp.array(pi0)))(jnp.array(new_trans_Ps_vmap), jnp.array(new_lls_jax_vmap))
+    return comp_LLloss(pi0, new_trans_Ps_vmap, new_lls_jax_vmap) / (all_xohs.shape[0]*all_xohs.shape[1]), \
+        comp_LLloss(pi0, new_trans_Ps_vmap_train, new_lls_jax_vmap_train) / (train_xohs.shape[0]*train_xohs.shape[1]),\
+        comp_LLloss(pi0, new_trans_Ps_vmap_test, new_lls_jax_vmap_test) / (test_xohs.shape[0]*test_xohs.shape[1]), jax_path_vmap
+
+def learnt_LL2(logpi0, log_Ps, Rs, rewards, temps, n_states, n_actions, trans_probs, all_xohs, all_aohs, train_xohs, train_aohs, test_aohs, test_xohs):
+    n_states, n_actions, _ = new_trans_probs.shape
+    pi0 = jnp.exp(logpi0 - jax_logsumexp(logpi0))
+    rewards_sa = jnp.expand_dims(rewards[:, 0, :], axis=2) * np.ones((1, n_states, n_actions))
+    pi, _, _ = vmap(partial(vi_temp, new_trans_probs))(rewards_sa, temps)
+    logemit = jnp.log(pi)
+    new_lls_jax_vmap = vmap(partial(comp_ll_jax, logemit))(jnp.array(all_xohs2), jnp.array(all_aohs))
+    new_trans_Ps_vmap = vmap(partial(comp_transP, jnp.array(log_Ps), Rs))(jnp.array(all_xohs))
+    new_lls_jax_vmap_train = vmap(partial(comp_ll_jax, logemit))(jnp.array(train_xohs2), jnp.array(train_aohs))
+    new_trans_Ps_vmap_train = vmap(partial(comp_transP, jnp.array(log_Ps), Rs))(jnp.array(train_xohs))
+    new_lls_jax_vmap_test = vmap(partial(comp_ll_jax, logemit))(jnp.array(test_xohs2), jnp.array(test_aohs))
+    new_trans_Ps_vmap_test = vmap(partial(comp_transP, jnp.array(log_Ps), Rs))(jnp.array(test_xohs))
+    jax_path_vmap = vmap(partial(_viterbi_JAX, jnp.array(pi0)))(jnp.array(new_trans_Ps_vmap), jnp.array(new_lls_jax_vmap))
+    return comp_LLloss(pi0, new_trans_Ps_vmap, new_lls_jax_vmap) / (all_xohs.shape[0]*all_xohs.shape[1]), comp_LLloss(pi0, new_trans_Ps_vmap_train, new_lls_jax_vmap_train) / (train_xohs.shape[0]*train_xohs.shape[1]), comp_LLloss(pi0, new_trans_Ps_vmap_test, new_lls_jax_vmap_test) / (test_xohs.shape[0]*test_xohs.shape[1]), jax_path_vmap
+
+
