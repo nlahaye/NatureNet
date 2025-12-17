45dd08fda513e2738c1c10227e50bbb3c56282d6
nlahaye <nlahaye@jpl.nasa.gov>
2025-12-17 03:22:55 +0000

    Minor updates to logic and removing print statements

--- src/naturenet/models/feature_rep/merge_env_maps.py
+++ src/naturenet/models/feature_rep/merge_env_maps.py
@@ -20,51 +20,45 @@ yml_conf = read_yaml(args.yaml)
 movement_df_fname = yml_conf["movement_df"]
 with open(movement_df_fname, 'rb') as f:
     movement_dfs = pickle.load(f)
-  
+ 
+scenes_per_uids = []
+for i in range(len(yml_conf["env_maps"])):
+    with open(yml_conf["env_maps"][i], 'rb') as f:
+        scenes_per_uids.append(pickle.load(f))
+ 
+ 
 for pm in range(len(yml_conf["primary_env_maps"])):
     primary_env_map = None
+    uids = [yml_conf["primary_env_maps"][pm][-12:-4]]
     primary_env_map_fname = os.path.join(yml_conf["primary_env_maps"][pm])
     with open(primary_env_map_fname, 'rb') as f:
         primary_env_map = pickle.load(f)
  
- 
-
-    scenes_per_uids = []
-    for i in range(len(yml_conf["env_maps"])):
-        with open(yml_conf["env_maps"][i], 'rb') as f:
-            scenes_per_uids.append(pickle.load(f))
-
 
     for i in range(len(scenes_per_uids)):
-        for uid in scenes_per_uids[i]:
+        for uid in uids:
             if True:
                 scenes_per_uid = scenes_per_uids[i][uid]
                 for j in range(len(scenes_per_uid)):
                     scenes_per_df = scenes_per_uid[j]
                     if len(primary_env_map) == j:
-                        print("ADDING SCENES", uid, j)
                         primary_env_map.append(scenes_per_df)
                         continue
                     elif len(primary_env_map[j]) == 0:
-                        print("UPDATING SCENES", uid, j)
                         primary_env_map[j] = scenes_per_df
                         continue
                     else:
                         for k in range(len(scenes_per_df)):
-                            print(len(primary_env_map[j]), k)
                             if len(primary_env_map[j]) == k:
                                 primary_env_map[j].append(scenes_per_df[k])
-                                print("ADDING SCENE", uid, j, k)
                                 continue
                             elif len(primary_env_map[j][k]) == 0:
                                 primary_env_map[j][k] = scenes_per_df[k]
-                                print("UPDATING SCENE", uid, j, k)
                                 continue
                             else:
                                 good = True
                                 for m in range(len(scenes_per_df[k])):
                                     if not hasattr(primary_env_map[j][k][m], "shape"):
-                                        print("UPDATING SCENE EMBED", uid, j, k, m)
                                         primary_env_map[j][k][m] = scenes_per_df[k][m]
                                         good = False
                                         continue
@@ -73,11 +67,11 @@ for pm in range(len(yml_conf["primary_env_maps"])):
 
 
 
+    
     if "uid" in yml_conf and len(yml_conf["uid"]) > pm:
         uid =  yml_conf["uid"][pm]
     else:
         uid = yml_conf["primary_env_maps"][pm][-12:-4]
-    print("UID", yml_conf["primary_env_maps"][pm], uid)
     complete = []
     missing = []
     for j in range(len(primary_env_map)):
@@ -101,7 +95,7 @@ for pm in range(len(yml_conf["primary_env_maps"])):
 
     if "uid" in yml_conf and len(yml_conf["uid"]) > pm:
         fname = os.path.join(os.path.dirname(yml_conf["primary_env_maps"][pm]), "env_map_" + yml_conf["uid"][pm] + ".pkl")
-    else
+    else:
         fname = os.path.join(yml_conf["primary_env_maps"][pm])
  
     with open(fname, 'wb') as f:
